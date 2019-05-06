49ef2bb6d47ad244005b9062da06edf55d6a96a8
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-05-06 22:44:49 +0800

    Fixed: https://github.com/linux-remote/linux-remote/issues/62

--- api/ws-ps.js
+++ api/ws-ps.js
@@ -3,18 +3,25 @@ const {spawn} = require('child_process');
 const WebSocket = require('ws');
 const {DebounceTime} = require('../common/debounce-time');
 const { safeSend } = require('./util');
-
+const zlib = require('zlib');
 const wsServer = new WebSocket.Server({ noServer: true });
 
 
 wsServer.on('connection', function connection(ws) {
-  console.log('ws-ps connection');
+  console.log('ws-ps connection2');
   const l = spawn('top', ['-b']);
   let out = '';
   const debounce = new DebounceTime(function(){
-    safeSend(ws, out);
-    out = '';
-  }, 200);
+    zlib.gzip(out, function(err, result){
+      if(err){
+        ws.close(1000, err.message);
+      } else {
+        safeSend(ws, result);
+      }
+      out = '';
+    })
+
+  }, 500);
 
   l.stdout.on('data', function(buffer) {
     // console.log('buffer', buffer);
