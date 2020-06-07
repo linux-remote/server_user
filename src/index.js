faa220c8ab9da16183bb3b19fbcb32b64f006b1e
hezedu <hezedu@hotmail.com>
2020-06-07 14:13:04 +0800

    Fixed: CLI linux-reload fail BUG.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const os = require('os');
+const fs = require('fs');
 const path = require('path');
 const userInfo = os.userInfo();
 
@@ -13,6 +14,31 @@ CONF.arrSrExitKey = 0;
 global._AFR_TIMEOUT__ =  global.IS_PRO ? 1000 * 60 * 15 : 1000 * 60 * 15 * 100;
 // console.log('global._AFR_TIMEOUT__', global._AFR_TIMEOUT__)
 global.RECYCLE_BIN_PATH = path.join(CONF.hiddenRootDir, 'recycle-bin');
-global._MAIN_PORT__ = os.tmpdir() + '/linux-remote-server_main.sock';
+global._MAIN_PORT__ = (global.IS_PRO ? os.tmpdir() : '/dev/shm') + '/linux-remote-server_main.sock';
 
+if(!userInfo.homedir){
+  console.error('Error: not have homedir');
+  return;
+}
+if(userInfo.homedir === '/'){
+  console.error('Error: homedir cannot be ' + userInfo.homedir);
+  return;
+}
+
+try {
+  fs.statSync(CONF.hiddenRootDir);
+} catch(err){
+  if(err.code === 'ENOENT'){
+    try {
+      fs.mkdirSync(CONF.hiddenRootDir);
+      fs.mkdirSync(global.RECYCLE_BIN_PATH);
+    } catch(err) {
+      console.error(err.name + ": " + err.message);
+      return;
+    }
+  } else {
+    console.error(err.name + ": " + err.message);
+    return;
+  }
+}
 require('./net-client.js');
--- src/net-client.js
+++ src/net-client.js
@@ -1,93 +1,110 @@
-const fs = require('fs');
+
 const net = require('net');
 const handleJsonData = require('./handle-json-data/index.js');
 // const handleNormal = require('./handle-normal.js');
 
-const CONF = global.CONF;
-const userInfo = global.__USER_INFO__;
-if(!userInfo.homedir){
-  console.error('Error: not have homedir');
-  return;
-}
-if(userInfo.homedir === '/'){
-  console.error('Error: homedir cannot be ' + userInfo.homedir);
-  return;
-}
-
-try {
-  fs.statSync(CONF.hiddenRootDir);
-} catch(err){
-  if(err.code === 'ENOENT'){
-    try {
-      fs.mkdirSync(CONF.hiddenRootDir);
-      fs.mkdirSync(global.RECYCLE_BIN_PATH);
-    } catch(err) {
-      console.error(err.name + ": " + err.message);
-      return;
-    }
-  } else {
-    console.error(err.name + ": " + err.message);
-    return;
-  }
-}
-
+const ppid = process.ppid;
+let sid;
+let isConnect = false;
+let reConnectCount = 0;
+const reConnectMax = 5;
+const reConnectInterval = 1500;
+let reConnectTimer = null;
 function init(){
 
-  function _dataListener(data){
-    if(data === 'ok'){
-      end(null, client);
+  const client = net.createConnection(global._MAIN_PORT__, () => {
+    isConnect = true;
+    reConnectCount = 0;
+    if(reConnectTimer){
+      clearTimeout(reConnectTimer);
+    }
+
+    client.setEncoding('utf-8');
+    if(sid){
+      client.write(sid + ' ' + global.__USER_INFO__.username);
     } else {
-      end(new Error('403'));
+      client.write(process.env.LR_ONCE_TOKEN);
+    }
+    client.once('data', _firstDataListener);
+  });
+
+  client.once('error', _beforeErrListener);
+  function _beforeErrListener(){
+    if(!isConnect){
+      reConnect();
     }
   }
-  function _errListener(err){
-    end(err);
+
+  function reConnect(){
+    if(isConnect || reConnectCount >= reConnectMax){
+      reConnectTimer = null;
+      return;
+    }
+    console.log('reConnectCount', reConnectCount)
+    reConnectTimer = setTimeout(function(){
+      init();
+      reConnectCount = reConnectCount + 1;
+      reConnect();
+    }, reConnectInterval);
   }
+
+  function _firstDataListener(data){
+    let msgs = data.split(' ');
+    if(msgs[0] === 'ok'){
+      if(!sid){
+        if(!msgs[1]){
+          throw new Error('un have sid.');
+        }
+        sid = msgs[1];
+      }
+      _end(null, client);
+    } else {
+      _end(new Error('403'));
+    }
+  }
+
+
 
   let isEnd = false;
-  let timer = null;
-
-  const client = net.createConnection(global._MAIN_PORT__, () => {
-    client.setEncoding('utf-8');
-    client.setNoDelay();
-    client.write(process.env.LR_ONCE_TOKEN);
-    client.once('data', _dataListener);
-  });
-  client.once('error', _errListener);
-  timer = setTimeout(function(){
-    timer = null;
-    client.destroy();
-    end(new Error('userServerConnectTimeout'));
-  }, 5000);
-  function end(err){
-    if(timer){
-      clearTimeout(timer);
-    }
+  
+  
+  function _end(err){
     if(isEnd){
       return;
     }
     isEnd = true;
 
-    client.off('data', _dataListener);
-    client.off('error', _errListener);
+    client.off('error', _beforeErrListener);
+
     if(err){
-      console.error('\n----------------------- user process create fail -----------------------\n', err)
+      console.error('user process create fail', err)
       return;
     }
-
-    console.log('\n----------------------- user process create success -----------------------\n');
+    console.log('user process create success');
     client.setTimeout(global._AFR_TIMEOUT__);
     client.on('timeout', () => {
-      global.__SOCKET_REQUEST__.request([CONF.arrSrExitKey, 'timeout']);
+      global.__SOCKET_REQUEST__.request([global.CONF.arrSrExitKey, 'timeout']);
       console.log('socket timeout');
       client.end(() => {
         console.log('\n----------------- process.exit -----------------\n');
         process.exit();
       });
     });
+    client.on('error', function(err){
+      console.error('handle client error', err);
+    })
     handleJsonData(client);
-    
   }
+
+
+  client.on('close', function(){
+    if(process.ppid === ppid){
+      // init();
+      isConnect = false;
+      reConnect();
+    }
+  })
 }
 
 init();
+
